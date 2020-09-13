718a186ce987a52bb0bce00e21f3c396260f256c
Jean-Baptiste Zeller <esurnir@gmail.com>
2020-09-13 21:39:19 +0200

    adding event/id route to my vue application.

---
+++ pages/event/_id.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Event #{{ id }}</h1>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  computed: {
+    id(): string {
+      return this.$route.params.id;
+    },
+  },
+  head() {
+    return {
+      title: "Event #" + this.id,
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "You can create a new event in your neighbourhood",
+        },
+      ],
+    };
+  },
+});
+</script>
