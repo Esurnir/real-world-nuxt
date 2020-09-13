bebcab993c191d116fc7e2b7edc8e891438cf466
Jean-Baptiste Zeller <esurnir@gmail.com>
2020-09-13 21:01:38 +0200

    Adding meta descriptions

---
+++ pages/event/create.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Create an Event</h1>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  head() {
+    return {
+      title: "Create an Event",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "You can create a new event in your neighbourhood",
+        },
+      ],
+    };
+  },
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,3 +3,13 @@
     <h1>Events</h1>
   </div>
 </template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  head() {
+    return {
+      title: "Event Listing",
+    };
+  },
+});
+</script>
